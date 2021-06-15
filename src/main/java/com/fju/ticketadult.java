51411e237a5d6115d67d657a30433f290296ffd0
louis <louis31084@gmail.com>
2021-06-16 02:23:39 +0800

    鄭樵 406262149

---
+++ src/main/java/com/fju/ticketadult.java
@@ -0,0 +1,10 @@
+package com.fju;
+
+public class ticketadult extends Ticket {
+    public ticketadult(){
+        id=2;
+        name= "pervert Circus - Ticket for Adult";
+        price =  1500;
+
+    }
+}
---
+++ src/main/java/com/fju/ticketkid.java
@@ -0,0 +1,13 @@
+package com.fju;
+
+public class ticketkid extends Ticket {
+    public ticketkid(){
+        id=1;
+        name= "pervert Circus - Ticket for Child";
+        price = 600;
+
+    }
+
+
+
+}
